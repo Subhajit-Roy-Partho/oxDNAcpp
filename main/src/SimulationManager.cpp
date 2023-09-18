ced3cfe7610510129008b8f284b6c62922510d2d
Subhajit-Roy-Partho <me.subhajitroy1999@gmail.com>
2023-09-17 23:00:05 -0700

    update

--- main/src/SimulationManager.cpp
+++ main/src/SimulationManager.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include "yaml-cpp/yaml.h" //This is an external library
-
+using namespace std;
 class Manager{
-    Manager(std::string configPath="")
-}
+public:
+    string configPath,projectName;
+    Manager(string configPath=""){
+        this->configPath=configPath;
+        // if(configPath !="") readYAML();
+    };
+
+    bool readYAML(){
+        YAML::Node config = YAML::LoadFile(configPath);
+        projectName = config["project"].as<string>();
+        cout<<"called"<<endl;
+        // cout<<projectName<<"\n";
+        return true;
+    };
+};
--- main/src/terminal.cpp
+++ main/src/terminal.cpp
@@ -1,6 +1,26 @@
+/*
+    Usage: terminal manager --config "location"
+*/
+
 #include "Analysis.cpp"
-int main(){
+#include "SimulationManager.cpp"
+
+
+int main(int argc, char *argv[]){
     // Analysis randPart("","","newCrystal");
-    std::cout<< "working"<<std::endl;
+
+
+
+    if(!std::strcmp(argv[1],"manager")){
+        for(int i=2;i<argc;i++){
+            if(!std::strcmp(argv[i],"--config")){
+                Manager manage(std::string(argv[i+1]));
+                manage.readYAML();
+            };
+        }
+    }
+
+
+    // std::cout<< "working"<<std::endl;
     return 0;
 }
