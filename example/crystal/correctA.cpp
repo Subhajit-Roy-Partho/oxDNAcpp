c5eb26ad90d780ce83fc77c258146331db827c57
Subhajit-Roy-Partho <me.subhajitroy1999@gmail.com>
2023-09-21 20:42:58 -0700

    update

--- example/crystal/correctA.cpp
+++ example/crystal/correctA.cpp
@@ -2,7 +2,7 @@
 
 int main(int argNum, char *argv[]) {
 
-  Analysis crystal("output.top", "input.dat", "crystal");
+  Analysis crystal("output.top", "test.dat", "crystal");
   crystal.correctA("./4crystals/newTest.dat");
   crystal.writeConfig("inputNew.dat");
   return 0;
