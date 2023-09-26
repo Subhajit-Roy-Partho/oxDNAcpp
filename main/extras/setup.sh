482f6585b6b78eaf279e7fbf20c9863ffddc3e09
Subhajit Roy <40741607+Subhajit-Roy-Partho@users.noreply.github.com>
2023-09-25 22:20:55 -0700

    Update setup.sh

--- main/extras/setup.sh
+++ main/extras/setup.sh
@@ -1,5 +1,5 @@
 sudo apt install git tmux -y
-sudo install gcc g++ python3 cmake make -y
+sudo apt install gcc g++ python3 cmake make -y
 git clone https://github.com/jbeder/yaml-cpp.git
 git clone https://github.com/Subhajit-Roy-Partho/oxDNAcpp.git
 git clone https://github.com/Subhajit-Roy-Partho/oxDNAold.git
