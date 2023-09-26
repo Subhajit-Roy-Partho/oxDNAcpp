d9cfb95509e25aa800a03d2c51c97c7eb13276c8
Subhajit-Roy-Partho <me.subhajitroy1999@gmail.com>
2023-09-25 22:18:14 -0700

    update

--- main/extras/setup.sh
+++ main/extras/setup.sh
@@ -1,9 +1,9 @@
-sudo apt install git -y
-sudo install gcc g++ python3 cmake make -y &
-git clone https://github.com/jbeder/yaml-cpp.git &
-git clone https://github.com/Subhajit-Roy-Partho/oxDNAcpp.git &
+sudo apt install git tmux -y
+sudo install gcc g++ python3 cmake make -y
+git clone https://github.com/jbeder/yaml-cpp.git
+git clone https://github.com/Subhajit-Roy-Partho/oxDNAcpp.git
 git clone https://github.com/Subhajit-Roy-Partho/oxDNAold.git
 
-cd oxDNAold; mkdir build; cd build; cmake ..; make -j$nproc &
+cd oxDNAold; mkdir build; cd build; cmake ..; make -j$nproc; make romano -j$nproc; echo "PATH=$PATH:'$pwd'" >> ~/.bashrc; echo "shopt -s autocd";
 cd ~/yaml-cpp; mkdir build; cd build; cmake ..; make -j$nproc; sudo make install;
 cd oxDNAold/main/src
