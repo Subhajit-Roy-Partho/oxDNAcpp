9684b97c19a69bff015166cc1987dcb58a69c3cb
Subhajit-Roy-Partho <me.subhajitroy1999@gmail.com>
2023-10-03 16:25:17 -0700

    update

--- main/extras/setup.sh
+++ main/extras/setup.sh
@@ -1,9 +1,9 @@
 sudo apt install git tmux -y
 sudo apt install gcc g++ python3 cmake make libeigen3-dev htop -y
-git clone https://github.com/jbeder/yaml-cpp.git
-git clone https://github.com/Subhajit-Roy-Partho/oxDNAcpp.git
-git clone https://github.com/Subhajit-Roy-Partho/oxDNAold.git
+git clone https://github.com/jbeder/yaml-cpp.git &
+git clone https://github.com/Subhajit-Roy-Partho/oxDNAcpp.git &
+git clone https://github.com/Subhajit-Roy-Partho/oxDNAold.git &
 
 cd oxDNAold; mkdir build; cd build; cmake ..; make -j$nproc; make romano -j$nproc;cd bin;echo "export PATH=$PATH:$(readlink -f .)" >> ~/.bashrc; echo "shopt -s autocd" >> ~/.bashrc;source ~/.bashrc;
 cd ~/yaml-cpp; mkdir build; cd build; cmake ..; make -j$nproc; sudo make install;
-cd oxDNAold/main/src
+cd oxDNAcpp/main/src;mkdir build; cd build; cmake ..; make -j$nproc
