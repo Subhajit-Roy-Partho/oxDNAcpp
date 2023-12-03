141d04d85dda50405ebce68f4e3b19a094454df5
Subhajit-Roy-Partho <me.subhajitroy1999@gmail.com>
2023-12-03 09:31:53 -0700

    update

--- main/managerExample/display.js
+++ main/managerExample/display.js
@@ -6,6 +6,7 @@ let patche0 =[[0,0.262866,0.4253250],
           [0, 0.262866, -0.4253250],
           [-0.425325, 0, -0.2628660]]
 let colors0=[21,22,23,24,25,26];
+let colors=[[255,102,102],[51,119,255],[204,204,0],[43,255,0],[204,102,255],[51,255,221]]
 let m=[]
 let i=0;
 patche0.forEach(vec => {
@@ -15,7 +16,7 @@ patche0.forEach(vec => {
     m[i] = new THREE.Mesh(
     new THREE.SphereGeometry(5,64,64),
     new THREE.MeshPhongMaterial(
-      {color: new THREE.Color(1,0,0),
+      {color: new THREE.Color(colors[i][0]/256.0,colors[i][1]/256.0,colors[i][2]/256.0),
         opacity:.5
       }
     )
